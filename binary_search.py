4d7fc252b07bf328f3e700bb497511f6efc322ce
cegrell <niklashojman@gmail.com>
2021-11-27 11:37:46 +0000

    binary search implementation

---
+++ binary_search.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def binary_search(arr: List[int], target: int) -> int:
+    left, right = 0, len(arr)-1
+    while left < right:
+        mid = left + (right - left) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+arr = [-1, 4, 5, 8, 10, 12, 14, 15, 22]
+target = 15
+print(binary_search(arr, target))
