e7a96413ff1b10aa7449fbd835aa0d6af98c66ea
cegrell <niklashojman@gmail.com>
2021-11-28 10:45:32 +0000

    minor changes to quick sort implementation

--- quick_sort.py
+++ quick_sort.py
@@ -1,11 +1,11 @@
 from typing import List
 
-def swap(arr: List[int], i: int, j: int):
+def swap(arr: List[int], i: int, j: int) -> None:
     tmp = arr[i]
     arr[i] = arr[j]
     arr[j] = tmp
 
-def partition(arr: List[int], l: int, r: int):
+def partition(arr: List[int], l: int, r: int) -> None:
     i, p = l-1, arr[r]
     for j in range(l, p):
         if arr[j] < p:
@@ -15,14 +15,13 @@ def partition(arr: List[int], l: int, r: int):
     swap(arr, i, r)
     return i
 
-def qs(arr: List[int], l, r) -> List[int]:
+def qs(arr: List[int], l, r) -> None:
     if l >= r:
         return
     p = partition(arr, l, r)
-
     qs(arr, l, p - 1)
     qs(arr, p + 1, r)
-    return arr
 
 arr = [5, 1, -2, 6, 8, 10, 2, 4]
-print(qs(arr, 0, len(arr)-1))
+qs(arr, 0, len(arr)-1)
+print(arr)
