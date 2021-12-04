fe5ddeff5bf14361d4f89bb1de285bca3d9ec5fc
cegrell <niklashojman@gmail.com>
2021-12-04 12:57:36 +0000

    Can construct

--- can_construct.py
+++ can_construct.py
@@ -1,4 +1,8 @@
 def can_construct(target: str, word_bank: list) -> bool:
+    # n = elements in word_bank - the branching factor
+    # m = target length, number of levels 
+    # Time: n^m * m
+    # Space: m^2 - since we're creating slices for each level in the tree 
     if target == '': return True
     target_length = len(target)
     for word in word_bank:
@@ -6,13 +10,35 @@ def can_construct(target: str, word_bank: list) -> bool:
         if w_len > target_length:
             continue
         if target[:w_len] == word:
-            if can_construct(target[w_len:], word_bank) is True:
-                return True
+            if can_construct(target[w_len:], word_bank) is True: # This line adds to the time complexity with *m
+                return True # above line creates a new string, which will be dependent on m, thus creating m * m
         elif target[w_len:] == word:
             if can_construct(target[:w_len], word_bank) is True:
                 return True
     return False
 
 
-print(can_construct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']))
+#print(can_construct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'ab', 'abc', 'cd', 'def', 'abcd']))
 
+def can_construct_optimised(target: str, word_bank: list, memo: dict = {}):
+    # With memoization
+    if target == '': return True
+    if target in memo: return memo[target]
+    target_length = len(target)
+    for word in word_bank:
+        w_len = len(word)
+        if w_len > target_length:
+            continue
+        if target[:w_len] == word:
+            if can_construct_optimised(target[w_len:], word_bank, memo) is True:
+                memo[target] = True
+                return True 
+        elif target[w_len:] == word:
+            if can_construct_optimised(target[:w_len], word_bank, memo) is True:
+                memo[target] = True
+                return True
+    memo[target] = False
+    return False
+
+print(can_construct_optimised('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', ['e', 'ee', 'eee', 'ab', 'abc', 'cd', 'def', 'abcd']))
+#print(can_construct_optimised('abcdefe', ['ab', 'abc', 'cd', 'def', 'abcd']))
