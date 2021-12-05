e413227ac78e4c546b61e815df77ce5cbb22f13e
cegrell <niklashojman@gmail.com>
2021-12-05 21:05:52 +0000

    all construct tabulation

--- all_construct.py
+++ all_construct.py
@@ -1,6 +1,6 @@
 from typing import List
 
-def all_construct(target: str, word_bank: list, memo={}) -> List[List[str]]:
+def all_construct_memo(target: str, word_bank: list, memo={}) -> List[List[str]]:
     # m lenght of target
     # b length of word_bnka
     # Time: n^m 
@@ -13,7 +13,7 @@ def all_construct(target: str, word_bank: list, memo={}) -> List[List[str]]:
     for word in word_bank:
         if target.find(word) == 0:
             suffix = target.replace(word, '')
-            ret = all_construct(suffix, word_bank)
+            ret = all_construct_memo(suffix, word_bank)
             for i in range(len(ret)):
                 ret[i].insert(0, word)  
             lst_collection.extend(ret)            
@@ -21,4 +21,16 @@ def all_construct(target: str, word_bank: list, memo={}) -> List[List[str]]:
     return lst_collection
 
 
-print(all_construct('abcdef', ['ab', 'c', 'def', 'd', 'ef', 'e', 'f']))
+# print() ('abcdef', ['ab', 'c', 'def', 'd', 'ef', 'e', 'f']))
+
+def all_construct_tabulation(target: str, word_bank: list) -> list:
+    tbl = [[] for _ in range(len(target) + 1)]
+    tbl[0] = [[]]
+    for i in range(len(target) + 1):
+        for word in word_bank:
+            if word == target[i:i+len(word)]:
+                new_combos = [x + [word] for x in tbl[i]]
+                tbl[i + len(word)] += new_combos                
+    return tbl[len(target)]
+
+print(all_construct_tabulation('abcdef', ['ab', 'c', 'def', 'd', 'ef', 'e', 'f']))
